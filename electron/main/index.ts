721b973cae250817506565a50c323d6964a5e6bb
lichong <wslcwslc@126.com>
2023-03-23 18:09:00 +0800

    feat: 添加ignore

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,19 +1,26 @@
 import { app, BrowserWindow } from 'electron';
+import path from 'path';
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 300,
-    height: 200,
+    width: 1000,
+    height: 800,
     webPreferences: {
       sandbox: true,
       // 是否开启隔离上下文
       contextIsolation: true,
       // 渲染进程使用Node API
       nodeIntegration: false,
+      preload: '../main/index.js',
     },
   });
-  const url = 'http://localhost:5173/#/';
-  win.loadURL(url);
+  if (app.isPackaged) {
+    win.loadFile(path.join(__dirname, './index.html'), { hash: 'login' });
+  } else {
+    const url = 'http://localhost:5173/';
+    win.webContents.openDevTools();
+    win.loadURL(url);
+  }
 };
 
 app.whenReady().then(() => {
